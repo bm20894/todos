fb7fcb7ed269b28a450ce95515e36aa0b7bdc814
bm20894 <milesboz@me.com>
2019-06-25 17:12:57 -0500

    changed title

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,9 @@ function About() {
 	return (
 		<React.Fragment>
 			<h1>About</h1>
-			<p>This is the TodoList app version 1.0.0. It is part of a React Crash Course.</p>
+			<p>
+				This is the TodoList app version 1.0.0. It is part of a React Crash Course.
+		</p>
 		</React.Fragment>
 	);
 }
