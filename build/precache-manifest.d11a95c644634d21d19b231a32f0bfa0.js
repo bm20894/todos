62b0d4f071133bdbf2577444b900ea52c7510d0c
bm20894 <milesboz@me.com>
2019-06-25 15:38:07 -0500

    first commit

---
+++ build/precache-manifest.d11a95c644634d21d19b231a32f0bfa0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b396ccc113e9a997a772687be2b08cb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f67807ff62f4917b75d",
+    "url": "/static/css/2.04aae6c4.chunk.css"
+  },
+  {
+    "revision": "c9c7af1920880982124c",
+    "url": "/static/css/main.6f30deb2.chunk.css"
+  },
+  {
+    "revision": "6f67807ff62f4917b75d",
+    "url": "/static/js/2.b8a4773d.chunk.js"
+  },
+  {
+    "revision": "c9c7af1920880982124c",
+    "url": "/static/js/main.c671254d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Header() {
+	return (
+		<header>
+			<h1>TodoList</h1>
+			<div className="head-text">
+				<Link className="head-link" to="/">Home</Link>
+				<span>|</span>
+				<Link className="head-link" to="/about">About</Link>
+			</div>
+				<br />
+				{" "}
+		</header>
+	);
+}
+
+export default Header
