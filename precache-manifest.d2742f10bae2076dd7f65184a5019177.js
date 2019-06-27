8f214eaf606bba3f186d2fa193ad9fec6c5ed335
bm20894 <milesboz@me.com>
2019-06-27 17:44:55 -0500

    Updates

--- precache-manifest.d2742f10bae2076dd7f65184a5019177.js
+++ precache-manifest.d2742f10bae2076dd7f65184a5019177.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b530085544e5ce42372cd34455eb959f",
+    "revision": "6b4896b6b55e0f46434589797d79ae99",
     "url": "/todos/index.html"
   },
   {
-    "revision": "6f67807ff62f4917b75d",
+    "revision": "79067dcffeb285ab99ac",
     "url": "/todos/static/css/2.04aae6c4.chunk.css"
   },
   {
-    "revision": "e5d3923b9c8b4b75d463",
+    "revision": "7a459d66f44a648b6616",
     "url": "/todos/static/css/main.ace0d4f4.chunk.css"
   },
   {
-    "revision": "6f67807ff62f4917b75d",
-    "url": "/todos/static/js/2.b8a4773d.chunk.js"
+    "revision": "79067dcffeb285ab99ac",
+    "url": "/todos/static/js/2.b1eacdd2.chunk.js"
   },
   {
-    "revision": "e5d3923b9c8b4b75d463",
-    "url": "/todos/static/js/main.6104f28e.chunk.js"
+    "revision": "7a459d66f44a648b6616",
+    "url": "/todos/static/js/main.65ad14b1.chunk.js"
   },
   {
     "revision": "89246cf4ecee7b6195cb",
@@ -26,5 +26,9 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "b154f72af31ac911b595f55f4d121b2e",
     "url": "/todos/static/media/img.b154f72a.png"
+  },
+  {
+    "revision": "5e02af51a044f8d6de136e2c7e8d6c9a",
+    "url": "/todos/static/media/spinner.5e02af51.gif"
   }
 ]);
